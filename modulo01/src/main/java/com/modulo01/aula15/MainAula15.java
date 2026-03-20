36a86f299dd2552d40ed73eee3d17724bd108dd8
samuel <samuka.jvm@gmail.com>
2026-03-20 00:45:43 -0400

    15 - Criando e acessando propriedades customizadas com @Value

--- modulo01/src/main/java/com/modulo01/aula15/MainAula15.java
+++ modulo01/src/main/java/com/modulo01/aula15/MainAula15.java
@@ -11,6 +11,8 @@ public class MainAula15 {
 	public static void main(String[] args) {
 		SpringApplication.run(MainAula15.class, args);
 		/* 15 - Criando e acessando propriedades customizadas com @Value
+		* Criar uma propriedade customizada no application.properties
+		* Anotando Classe de NotificadorEmail com @Value para acessar as propriedades customizadas
 		*/
 
 	}
